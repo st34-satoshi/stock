35cb2b0a3069ddecca6a182f8b32b7584d8451bb
satoshi-tanaka <st34.satoshi@gmail.com>
2020-10-25 11:47:27 +0900

    add data of 1321

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 start = datetime(2020, 1, 1)
 end = datetime(2020, 2, 1)
-brand = '9984.JP'
+brand = '1321.JP'
 
 stooq = StooqDailyReader(brand, start=start, end=end)
 data = stooq.read()  # pandas.core.frame.DataFrame
